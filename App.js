f08e62d15702e1dc07dcc0932649719fc429a5c1
Bismah-Jadoon <143310705+Bismah-Jadoon@users.noreply.github.com>
2023-10-19 21:01:48 +0500

    Props and PropTypes

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+   
+ <Navbar title = "TextUtils" about= "About TextUtils"/>
+{/* When we use default prop we use such type of calling */}
+    {/* <Navbar/> */}
+    </>
+
+  );
+}
+
+export default App;
